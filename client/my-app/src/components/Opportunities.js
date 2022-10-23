359392ccbf4d6e6d559a74c8f0706944f3be17e6
anders520 <anderschoy0604@gmail.com>
2022-10-22 19:29:44 -0700

    formatting chages to opp page

--- client/my-app/src/components/Opportunities.js
+++ client/my-app/src/components/Opportunities.js
@@ -1,5 +1,6 @@
 import React, {useRef} from "react"
 import {
+    Component,
     Accordion,
     AccordionItem,
     AccordionButton,
@@ -13,10 +14,10 @@ import {
 export default function Opportunities(){
     return (
         <>
-            <Box>
+            <Box left='50px' top='30px' position='fixed' fontSize={30}>
                 <h1>Opportunities Page</h1>
             </Box>
-            <Accordion allowToggle width={600} marginLeft={10} marginTop={100} marginRight='none'>
+            <Accordion allowToggle width={600} marginLeft={10} marginTop={100} marginRight='none' position='fixed'>
                 <AccordionItem>
                     <h2>
                         <AccordionButton height={24}>
@@ -86,7 +87,7 @@ export default function Opportunities(){
                 </AccordionItem>
             </Accordion>
             
-            <Box boxSize='sm' float='right'>
+            <Box width={"min-content"} right={100} top={120} position='absolute' border='black' borderWidth='10px' borderStyle='solid'>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d339923.40409647423!2d-74.19004066399376!3d40.11383543201792!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c19ce4a38e7105%3A0x3f5b6f5b358b3cb0!2sToms%20River%2C%20NJ!5e0!3m2!1sen!2sus!4v1666487049580!5m2!1sen!2sus" width="600" height="450" allowfullscreen="no" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
                 </iframe>
             </Box>
